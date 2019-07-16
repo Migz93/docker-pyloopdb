4581021629dc305fc19a6f5cf181c597a60d9294
Migz93 <33037112+Migz93@users.noreply.github.com>
2019-07-16 11:12:45 +0100

    Fixes
    Various fixes as a result of running script through http://pep8online.com.

--- run.py
+++ run.py
@@ -4,55 +4,58 @@ import logging
 import os
 from influxdb import InfluxDBClient
 
+
 def elec_trace():
-	elec_usaeage = le.electricity_usage * 1000;
+    elec_usaeage = le.electricity_usage * 1000
     json_elec = [
-    {
+      {
         "measurement": "W",
         "tags": {
             "domain": "sensor",
             "entity_id": "loopenergy_elec",
-			"friendly_name": "loopenergy_elec"
+            "friendly_name": "loopenergy_elec"
         },
         "fields": {
             "value": elec_usaeage
         }
-    }
+      }
     ]
     client.write_points(json_elec)
 
+
 def gas_trace():
     json_gas = [
-    {
+      {
         "measurement": "W",
         "tags": {
             "domain": "sensor",
             "entity_id": "loopenergy_gas",
-			"friendly_name": "loopenergy_gas"
+            "friendly_name": "loopenergy_gas"
         },
         "fields": {
             "value": le.gas_useage
         }
-    }
+      }
     ]
     client.write_points(json_gas)
 
-elec_serial = os.environ['elec_serial'];
-elec_secret = os.environ['elec_secret'];
-gas_serial = os.environ['gas_serial'];
-gas_secret = os.environ['gas_secret'];
-type = os.environ['type'];
-influxhost = os.environ['influxhost'];
-influxport = os.environ['influxport'];
-influxdb = os.environ['influxdb'];
+elec_serial = os.environ['elec_serial']
+elec_secret = os.environ['elec_secret']
+gas_serial = os.environ['gas_serial']
+gas_secret = os.environ['gas_secret']
+type = os.environ['type']
+influxhost = os.environ['influxhost']
+influxport = os.environ['influxport']
+influxdb = os.environ['influxdb']
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 client = InfluxDBClient(host=influxhost, port=influxport, database=influxdb)
 
 if type == "elec":
-  le = pyloopenergy.LoopEnergy(elec_serial, elec_secret)
-  le.subscribe_elecricity(elec_trace)
+    le = pyloopenergy.LoopEnergy(elec_serial, elec_secret)
+    le.subscribe_elecricity(elec_trace)
 elif type == "elec,gas":
-  le = pyloopenergy.LoopEnergy(elec_serial, elec_secret, gas_serial, gas_secret)
-  le.subscribe_elecricity(elec_trace)
-  le.subscribe_gas(gas_trace)
+    le = pyloopenergy.LoopEnergy(elec_serial, elec_secret,
+                                 gas_serial, gas_secret)
+    le.subscribe_elecricity(elec_trace)
+    le.subscribe_gas(gas_trace)
